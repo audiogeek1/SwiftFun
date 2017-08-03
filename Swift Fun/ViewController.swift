00d9e5cec9d2ff1d05c44f2269a0faf0f6ac7e2c
Unknown <audiogeek1@netscape.net>
2017-08-03 16:02:40 -0600

    Initial commit

---
+++ Swift Fun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift Fun
+//
+//  Created by Michael Maughan on 8/2/17.
+//  Copyright © 2017 Michael Maughan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
